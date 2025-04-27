390c98a0b14afb5448fc7eb1258a9a80ff653d0d
Josip Brljevic <josipbrljevic@gmail.com>
2025-04-27 16:08:05 +0200

    vite config2

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/PokedexAssignment/",
+  base: "/PokedexAssignment",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
 });
